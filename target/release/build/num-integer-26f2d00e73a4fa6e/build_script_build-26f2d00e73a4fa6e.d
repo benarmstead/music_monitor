dd63071c52a5f93cd867e8efff6b9597fa025ed8
Ben Armstead <benarmstead@protonmail.com>
2021-05-17 18:39:20 +0100

    Create release
    Former-commit-id: 2691e0ce7c29fe597a850c3c8d90e7385b02a395

---
+++ target/release/build/num-integer-26f2d00e73a4fa6e/build_script_build-26f2d00e73a4fa6e.d
@@ -0,0 +1,5 @@
+/home/ben/cmus/cmus-music-monitor-rs/target/release/build/num-integer-26f2d00e73a4fa6e/build_script_build-26f2d00e73a4fa6e: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/build.rs
+
+/home/ben/cmus/cmus-music-monitor-rs/target/release/build/num-integer-26f2d00e73a4fa6e/build_script_build-26f2d00e73a4fa6e.d: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/build.rs
+
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/build.rs:
---
+++ target/release/deps/time-9bf7043d4a3044d8.d
@@ -0,0 +1,11 @@
+/home/ben/cmus/cmus-music-monitor-rs/target/release/deps/time-9bf7043d4a3044d8.rmeta: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/lib.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/display.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/duration.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/parse.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/sys.rs
+
+/home/ben/cmus/cmus-music-monitor-rs/target/release/deps/libtime-9bf7043d4a3044d8.rlib: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/lib.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/display.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/duration.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/parse.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/sys.rs
+
+/home/ben/cmus/cmus-music-monitor-rs/target/release/deps/time-9bf7043d4a3044d8.d: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/lib.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/display.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/duration.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/parse.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/sys.rs
+
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/lib.rs:
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/display.rs:
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/duration.rs:
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/parse.rs:
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/time-0.1.44/src/sys.rs:
