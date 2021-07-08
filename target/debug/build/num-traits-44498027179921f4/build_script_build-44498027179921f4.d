a4979715571fbd77c4dd6ef0c5df213eca6f580b
Ben Armstead <benarmstead@protonmail.com>
2021-07-08 11:42:10 +0100

    Change .csv from hard-coded to being taken as cli argument

---
+++ target/debug/build/num-traits-44498027179921f4/build_script_build-44498027179921f4.d
@@ -0,0 +1,5 @@
+/home/ben/Documents/Programming/Completed/cmus-music-monitor-rs/target/debug/build/num-traits-44498027179921f4/build_script_build-44498027179921f4: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.14/build.rs
+
+/home/ben/Documents/Programming/Completed/cmus-music-monitor-rs/target/debug/build/num-traits-44498027179921f4/build_script_build-44498027179921f4.d: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.14/build.rs
+
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.14/build.rs:
---
+++ target/debug/deps/num_integer-9bc6616438f9f81e.d
@@ -0,0 +1,9 @@
+/home/ben/Documents/Programming/Completed/cmus-music-monitor-rs/target/debug/deps/num_integer-9bc6616438f9f81e.rmeta: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/lib.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/roots.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/average.rs
+
+/home/ben/Documents/Programming/Completed/cmus-music-monitor-rs/target/debug/deps/libnum_integer-9bc6616438f9f81e.rlib: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/lib.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/roots.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/average.rs
+
+/home/ben/Documents/Programming/Completed/cmus-music-monitor-rs/target/debug/deps/num_integer-9bc6616438f9f81e.d: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/lib.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/roots.rs /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/average.rs
+
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/lib.rs:
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/roots.rs:
+/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.44/src/average.rs:
